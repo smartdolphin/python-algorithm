fc9e895a8865fa30c0a96ef7b08c3ee9b02f6a9d
Tae ho Lee <smartdolphin07@gmail.com>
2018-10-06 10:34:20 +0900

    Add 'permutation'
    Signed-off-by: Tae ho Lee <smartdolphin07@gmail.com>

---
+++ algorithm/permutation.py
@@ -0,0 +1,37 @@
+import unittest
+
+
+def swap(arr, a, b):
+    arr[a], arr[b] = arr[b], arr[a]
+
+
+def permutation(arr, length, offset, result):
+    if offset == length:
+        result.append(arr[:])
+        return
+    for i in range(offset, length):
+        swap(arr, i, offset)
+        permutation(arr, length, offset + 1, result)
+        swap(arr, i, offset)
+
+
+class TestPermutation(unittest.TestCase):
+    def test(self):
+        arr = ['A', 'B', 'C']
+        result = []
+        permutation(arr, len(arr), 0, result)
+        self.assertEqual(
+            result,
+            [
+                ['A', 'B', 'C'],
+                ['A', 'C', 'B'],
+                ['B', 'A', 'C'],
+                ['B', 'C', 'A'],
+                ['C', 'B', 'A'],
+                ['C', 'A', 'B']
+            ]
+        )
+
+
+if __name__ == '__main__':
+    unittest.TestCase()
