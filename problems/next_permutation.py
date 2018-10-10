1ac48a9ef2b83a0d2165ab3665f487a843cec917
Tae ho Lee <smartdolphin07@gmail.com>
2018-10-10 23:22:09 +0900

    Fix bug for next permutation
    index error
    
    Signed-off-by: Tae ho Lee <smartdolphin07@gmail.com>

--- problems/next_permutation.py
+++ problems/next_permutation.py
@@ -14,11 +14,11 @@ class Solution:
         """
         size = len(nums)
         a = size - 2
-        while a >= 0 and nums[a] > nums[a + 1]:
+        while a >= 0 and nums[a] >= nums[a + 1]:
             a -= 1
         if a >= 0:
             b = size - 1
-            while b >= 0 and nums[a] > nums[b]:
+            while b >= 0 and nums[a] >= nums[b]:
                 b -= 1
             nums[a], nums[b] = nums[b], nums[a]
         start = a + 1
@@ -28,28 +28,23 @@ class Solution:
             nums[start], nums[end] = nums[end], nums[start]
             start += 1
             end -= 1
-        return nums
 
 
 class TestNextPermutation(unittest.TestCase):
     def test(self):
         sol = Solution()
-        self.assertEqual(
-            sol.nextPermutation([1, 2, 3]),
-            [1, 3, 2]
-        )
-        self.assertEqual(
-            sol.nextPermutation([3, 2, 1]),
-            [1, 2, 3]
-        )
-        self.assertEqual(
-            sol.nextPermutation([1, 1, 5]),
-            [1, 5, 1]
-        )
-        self.assertEqual(
-            sol.nextPermutation([2, 3, 1]),
-            [3, 1, 2]
-        )
+        nums = [1, 2, 3]
+        sol.nextPermutation(nums)
+        self.assertEqual(nums, [1, 3, 2])
+        nums = [3, 2, 1]
+        sol.nextPermutation(nums)
+        self.assertEqual(nums, [1, 2, 3])
+        nums = [1, 1, 5]
+        sol.nextPermutation(nums)
+        self.assertEqual(nums, [1, 5, 1])
+        nums = [2, 3, 1]
+        sol.nextPermutation(nums)
+        self.assertEqual(nums, [3, 1, 2])
 
 
 if __name__ == '__main__':
